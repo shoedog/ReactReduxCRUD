ccaf5076d2294432d57a7cc5c81856c1fd91a4cb
shoedog <wesley.jinks@gmail.com>
2016-10-02 19:50:41 -0600

    update

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 var hike = require('./routes/hike');
 
 var app = express();
